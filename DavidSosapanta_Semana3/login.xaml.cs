58deeb8b8c3504edbb9bd45d42fa3f574663e5e1
DavidSosapanta <dsosapanta@hotmail.com>
2021-04-29 06:18:10 +0200

    actualizacion

---
+++ DavidSosapanta_Semana3/login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DavidSosapanta_Semana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private  async void btnventana_Clicked(object sender, EventArgs e)
+        {
+            String usuario = txtUsuario.Text;
+            String clave = txtClave.Text;
+            if (usuario.Equals("estudiante2021") && clave.Equals("uisrael2021"))
+            {
+                await Navigation.PushAsync(new MainPage(usuario));
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Clave o usuario incorrectos", "OK");
+            }
+
+        }
+    }
+}
